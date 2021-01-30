5f4d31a3db1b219800bd03d97f365a0292490e11
wrh17b <wrh17b@my.fsu.edu>
2021-01-30 17:00:17 -0500

    3.create a branch where instead of "Hello World" it will print "Hello <YOUR NAME>" and push the branch to GitHub

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -7,7 +7,7 @@ IDE + GIT
 
 public class Hello_World {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Wesley Harris!");
     }
 
 }

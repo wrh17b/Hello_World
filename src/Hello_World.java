483abf4f6726f699fc2ab01e11f7c459ddbf91fe
wrh17b <wrh17b@my.fsu.edu>
2021-01-30 16:57:45 -0500

    Initial commit

---
+++ src/Hello_World.java
@@ -0,0 +1,13 @@
+/*
+Wesley Harris
+CEN4020
+ASSIGNMENT 1
+IDE + GIT
+ */
+
+public class Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
